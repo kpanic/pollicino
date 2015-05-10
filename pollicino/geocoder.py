2b27f8280859eef71f44ca8fd43af1a7e1799a2a
Marco Milanesi <kpanic@gmail.com>
2015-05-10 07:43:30 +0000

    Support ttl in the serialized document

--- pollicino/geocoder.py
+++ pollicino/geocoder.py
@@ -22,14 +22,24 @@ class OpenStreetMap(Nominatim):
 
 
 class Google(GoogleV3):
+
+    def __init__(self, *args, **kwargs):
+        self.index_ttl = kwargs.pop('ttl')
+        super(Google, self).__init__(*args, **kwargs)
+
     def geocode(self, query, **kwargs):
+        geocoded_data = []
         kwargs['exactly_one'] = False
+
         responses = super(Google, self).geocode(query, **kwargs)
         if responses:
             geo_serializer = GeocoderResponse(provider='google')
-            responses = [geo_serializer.serialize(response)
-                         for response in responses]
-        return responses
+            for response in responses:
+                serialized_address = geo_serializer.serialize(response)
+                serialized_address['ttl'] = self.index_ttl
+                geocoded_data.append(serialized_address)
+
+        return geocoded_data
 
 
 class GeoContainer(object):
