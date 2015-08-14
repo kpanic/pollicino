e4f081c21061afcae37db8daacbea639372c1d26
Marco Milanesi <kpanic@gmail.com>
2015-08-14 16:51:13 +0200

    Adapt the import script to use pbf2json output

--- scripts/import_addresses.py
+++ scripts/import_addresses.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import csv
-import sys
 from os.path import dirname, realpath
+import json
 
 from pollicino.store import Store
 from pollicino.config import CONFIG
@@ -12,48 +11,51 @@ store = Store.from_config(CONFIG)
 
 pwd = dirname(realpath(__file__))
 
-data_path = '%s/../data/excerpt_berlin_streets.csv' % pwd
-
-csv.field_size_limit(sys.maxsize)
+data_path = '%s/../data/excerpt_berlin_streets.json' % pwd
 
 
 def prepare_addresses(address_components):
     for component in address_components:
-        # If it has not street and coordinates are not float, skip
-        if not all([component['street'], component['city']]):
-            continue
+        component = json.loads(component)
+        tags = component.get('tags')
+        if tags is not None:
+            # If it has not street and coordinates are not float, skip
+            if not all([tags.get('addr:street'), tags.get('addr:city')]):
+                continue
 
-        address = {}
-        try:
-            lon = float(component['lon'].strip())
-            lat = float(component['lat'].strip())
-        except ValueError:
-            # Corrupted data, it's not a float, skip
-            continue
-        full_address = ' '.join([
-            component['street'],
-            component['housenumber'],
-            component['suburb'],
-            component['postcode'],
-            component['city'],
-            ])
+            address = {}
 
-        address.update({
-            "full_address": full_address,
-            "house_number": component['housenumber'],
-            "city": component['city'],
-            "suburb": component['suburb'],
-            "postcode": component['postcode'],
-            "road": component['street'],
-            "coordinates": [lon, lat]
-        })
-        yield address
+            try:
+                lon = float(component['lon'])
+                lat = float(component['lat'])
+            except KeyError:
+                centroid = component.get('centroid')
+                lon = float(centroid['lon'])
+                lat = float(centroid['lat'])
+
+            full_address = ' '.join([
+                tags.get('addr:street', ''),
+                tags.get('addr:housenumber', ''),
+                tags.get('addr:suburb', ''),
+                tags.get('addr:postcode', ''),
+                tags.get('addr:city', ''),
+                ])
+
+            address.update({
+                "full_address": full_address,
+                "house_number": tags.get('addr:housenumber', ''),
+                "city": tags.get('addr:city', ''),
+                "suburb": tags.get('addr:suburb', ''),
+                "postcode": tags.get('addr:postcode', ''),
+                "road": tags.get('addr:street', ''),
+                "coordinates": [lon, lat]
+            })
+            yield address
 
 
 with open(data_path) as fd:
-    reader = csv.DictReader(fd, delimiter=';')
 
-    addresses = prepare_addresses(reader)
+    addresses = prepare_addresses(fd)
 
     print("Bulk indexing...")
     store.bulk(addresses)
