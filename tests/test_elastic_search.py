35886fa8d52ba9ba7e153980163ce73a3ae3c01a
Marco Milanesi <kpanic@gmail.com>
2016-04-08 23:42:03 +0200

    Clean-up

--- tests/test_elastic_search.py
+++ tests/test_elastic_search.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import mock
 
-from pollicino import geocoder, exceptions
-from pollicino.store import Store
+from pollicino import exceptions
+from pollicino import store
 
-from pollicino import exceptions, store
 from elasticsearch import NotFoundError
 
 
 class ElasticsearchIntegrationTestCase(unittest.TestCase):
     def setUp(self):
-        self.geocoder_mock = mock.MagicMock()
-        self.store = store.Elasticsearch
-        self.store.index = 'test_pollicino'
-        self.store = self.store()
+        storage = store.Elasticsearch
+        storage.index = 'pollicino_test'
+        self.storage = storage()
 
-        address = {
+        self.address = {
             "full_address": "Landsberger Allee 70 10249 Berlin",
             "house_number": "70",
             "city": "Berlin",
@@ -26,10 +23,11 @@ class ElasticsearchIntegrationTestCase(unittest.TestCase):
             "road": "Landseberger Allee",
             "coordinates": [1.00, 1.002]
         }
-        self.store.set(address, id=1)
-        self.store.backend.indices.refresh(index=self.store.index)
 
-    def test_store_delete(self):
+        self.storage.set(self.address, id=1)
+        self.storage.backend.indices.refresh(index=self.storage.index)
+
+    def test_storage_delete(self):
         address = {
             "full_address": "Kiefholzstr 16 12435 Berlin",
             "house_number": "16",
@@ -38,34 +36,30 @@ class ElasticsearchIntegrationTestCase(unittest.TestCase):
             "postcode": "12435",
             "road": "Kiefholzstr",
             "coordinates": [1.00, 1.002]
-            }
-        self.store.set(address, id=2)
-        self.store.backend.indices.refresh(index=self.store.index)
-        self.store.delete(id=2)
-        with self.assertRaises(NotFoundError):
-            self.store.get(id=2)
+        }
 
-    def test_elasticssearch_real_address(self):
+        self.storage.set(address, id=2)
+        self.storage.backend.indices.refresh(index=self.storage.index)
+        self.storage.delete(id=2)
+        with self.assertRaises(NotFoundError):
+            self.storage.get(id=2)
+
+    def test_storage_with_a_real_address(self):
         address = "Landsberger Allee 70 10249 Berlin"
 
-        response = self.store.search(address)
+        response = self.storage.search(address)
 
-        # TODO:
-        # Landsberger Allee  70    10249  Berlin
-        # Some responses like the one above have too many spaces
-        # That's why the spliting is done
+        self.assertEqual(response[0], self.address)
 
-        # TODO:
-        # Is not matching exactly, even when an exact address is given
-        # the response contains multiple addresses
-        self.assertTrue("".join(address.split()) in
-                        "".join(res['full_address'].split())
-                        for res in response)
+    def test_storage_partial_complete(self):
+        address = "Lands"
 
-    def test_elasticssearch_fake_address(self):
+        response = self.storage.search(address)
+
+        self.assertEqual(response[0], self.address)
+
+    def test_storage_with_unexisting_address(self):
         address = "xxxxxx 70 100000 Berlin"
 
         with self.assertRaises(exceptions.StoreDataNotFound):
-            self.store.search(address)
-
-
+            self.storage.search(address)
